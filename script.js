54173bb4e4415fad2428d5b1c4ca09ff0e51a8a8
Ness <johnsondavid1107@gmail.com>
2024-04-07 12:03:37 -0400

    added favicon and final first draft push

--- script.js
+++ script.js
@@ -37,32 +37,58 @@ $(document).ready(function () {
         id: "goForwardPlan",
         animation: "fadeTo"
     },
+    {
+        id: "Fin",
+        animation: "fadeTo"
+    },
     ];
 
     var demoIndex = 0;
     var subSlideIndex = 0
+    var inTransition = false;
+
+
     $(document).on("keydown", function (e) {
 
-        if (demoIndex + 1 < demoArray.length) {
+        if (inTransition) {
+            return
+        }
+
+        if (demoIndex + 1 < demoArray.length && !inTransition) {
+            inTransition = true;
 
             //Go forward in slideshow
             if (e.code === "ArrowRight") {
                 if (demoArray[demoIndex]["subSlide"] && subSlideIndex < demoArray[demoIndex]["subSlide"].length) {
+
+                    $("#content").css("position", "relative");
                     $("#content").animate({
-                        "margin-right": "2000px"
+                        "right": "2000px"
+                    }, function () {
+                        $("#content").hide()
+                        $("#demo").show(function () {
+                            $("#demo").animate({
+                                "left": "-2000px"
+                            });
+                        });
+
                     });
+                    subSlideIndex++;
+                    inTransition = false
+
                     return
                 } else {
                     console.log("no slides to show")
                 }
 
                 $("#" + demoArray[demoIndex]["id"]).fadeOut("slow", function () {
+                    inTransition = true;
                     demoIndex++;
 
                     console.log("Demo Index is  " + demoIndex)
 
                     $("#" + demoArray[demoIndex]["id"]).fadeIn("slow", function () {
-
+                        inTransition = false;
                     });
                 });
 
@@ -75,11 +101,12 @@ $(document).ready(function () {
         if (demoIndex <= demoArray.length && demoIndex !== 0) {
             if (e.code === "ArrowLeft") {
                 $("#" + demoArray[demoIndex]["id"]).fadeOut("slow", function () {
+                    inTransition = true
                     demoIndex--;
 
                     console.log("Demo Index is  " + demoIndex)
                     $("#" + demoArray[demoIndex]["id"]).fadeIn("slow", function () {
-
+                        inTransition = false
                     });
                 });
             }
